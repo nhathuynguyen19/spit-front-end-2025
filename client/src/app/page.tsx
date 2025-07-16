6e7695870b0cf048aa894fbb163599c291c0ea7a
nhathuynguyen19 <huythcsthuyphuong73@gmail.com>
2025-07-16 12:02:04 +0700

    ngo mon

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,7 +1,16 @@
 'use client'
 
+import dynamic from 'next/dynamic'
+
+const NgoMonModel = dynamic(() => import('@/components/ngo-mon'), {
+    ssr: false,
+    loading: () => <p>Loading 3D...</p>,
+})
+
 export default function Home() {
   return (
-      <p>HELLO WORLD</p>
+      <main className="w-full h-screen">
+          <NgoMonModel />
+      </main>
   )
 }
