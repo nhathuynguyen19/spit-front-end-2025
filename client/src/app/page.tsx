a2fd10e37ad9add083d7721a849061315b79a7b2
nhathuynguyen19 <huythcsthuyphuong73@gmail.com>
2025-07-17 22:51:28 +0700

    update

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,11 +1,18 @@
 "use client";
 
 import Header from "@/components/Header";
+import dynamic from "next/dynamic";
+
+const NgoMonModel = dynamic(() => import("@/components/NgoMon"), {
+  ssr: false,
+  loading: () => <p>Loading 3D...</p>,
+});
 
 export default function Home() {
   return (
     <main>
-      <Header />
+      {/* <Header /> */}
+      <NgoMonModel />
     </main>
   );
 }
