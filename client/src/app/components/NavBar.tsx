c4c071f25a2f124cb1d057302d705cc4d1da323f
nhathuynguyen19 <huythcsthuyphuong73@gmail.com>
2025-07-21 16:20:11 +0700

    done co ban lang nghe

--- client/src/app/components/NavBar.tsx
+++ client/src/app/components/NavBar.tsx
@@ -6,10 +6,10 @@ import Button from "@/app/components/Button";
 const NavBar = () => {
   return (
     <section
-      className="fixed top-0 left-0 right-0 h-[50px] z-30 w-full flex items-center justify-center select-none shadow-lg backdrop-blur-sm"
+      className="fixed min-w-[1080px] top-0 left-0 right-0 h-[50px] z-30 w-full flex items-center justify-center select-none shadow-lg backdrop-blur-sm"
       draggable={false}
     >
-      <div className="mx-auto z-10 flex space-x-5 text-white md:text-base sm:text-[10px] text-[10px] tracking-wide">
+      <div className="mx-auto z-10 flex space-x-5 text-white text-base tracking-wide">
         <Button idName="trang-chu">Trang Chủ</Button>
         <Button idName="lang-nghe">Làng Nghề</Button>
         <Button>Ẩm Thực</Button>
