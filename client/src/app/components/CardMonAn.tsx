9b99249d663764b8fcd93d9923655c7ef29fc6dc
nhathuynguyen19 <huythcsthuyphuong73@gmail.com>
2025-08-10 10:51:12 +0700

    xong am thuc

---
+++ client/src/app/components/CardMonAn.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+type CardMonAnProps = {
+  src: string;
+  title: string;
+  des: string;
+};
+
+function CardMonAn({ src, title, des }: CardMonAnProps) {
+  return (
+    <div className="image-card h-full m-4 aspect-[1/1.5] transition-transform duration-300 ease-in-out cursor-pointer md:h-[calc(100%/1.5)] md:h-[100%]">
+      <div className="w-full aspect-square relative overflow-hidden rounded-2xl shadow-[2px_2px_4px_1px_rgba(0,0,0,0.3)]">
+        <img src={src} alt={title} className="h-full object-cover" />
+
+        <div className="overlayImage opacity-0 transition-opacity duration-300"></div>
+      </div>
+      <p className="text-black text-md font-bold cursor-text mt-2">{title}</p>
+      <p className="text-gray-700 text-[10px] sm:text-sm cursor-text italic">{des}</p>
+    </div>
+  );
+}
+
+export default CardMonAn;
