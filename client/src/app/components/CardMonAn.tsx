44795206e4c99f457b7a72a0411745a6480a6572
nhathuynguyen19 <huythcsthuyphuong73@gmail.com>
2025-08-12 06:13:01 +0700

    s

--- client/src/app/components/CardMonAn.tsx
+++ client/src/app/components/CardMonAn.tsx
@@ -9,8 +9,8 @@ type CardMonAnProps = {
 
 function CardMonAn({ src, title, des }: CardMonAnProps) {
   return (
-    <div className="image-card h-full m-4 aspect-[1/1.5] transition-transform duration-300 ease-in-out cursor-pointer md:h-[100%]">
-      <div className="overflow-hidden w-full aspect-square relative rounded-2xl shadow-[2px_2px_4px_1px_rgba(0,0,0,0.3)]">
+    <div className="h-full m-4 aspect-[1/1.5] transition-transform duration-300 ease-in-out cursor-pointer md:h-[100%]">
+      <div className="image-card overflow-hidden w-full aspect-square relative rounded-2xl shadow-[2px_2px_4px_1px_rgba(0,0,0,0.3)]">
         <img src={src} alt={title} className="h-full object-cover" />
 
         <div className="overlayImage opacity-0 transition-opacity duration-300"></div>
